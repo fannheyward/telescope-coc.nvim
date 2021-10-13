ce1cd182b9f176387ae2cf4fed16559a27c9ec73
wongxy <xiyao.wong@foxmail.com>
2021-10-13 14:09:14 +0800

    refactor

--- lua/telescope/_extensions/coc.lua
+++ lua/telescope/_extensions/coc.lua
@@ -1,41 +1,30 @@
-local actions = require('telescope.actions')
-local action_state = require('telescope.actions.state')
+local actions = require 'telescope.actions'
+local action_state = require 'telescope.actions.state'
 local conf = require('telescope.config').values
-local finders = require('telescope.finders')
-local make_entry = require('telescope.make_entry')
-local pickers = require('telescope.pickers')
-local entry_display = require('telescope.pickers.entry_display')
-local utils = require('telescope.utils')
-local Path = require('plenary.path')
-local string = string
----@diagnostic disable-next-line: undefined-global
-local vim = vim
----@diagnostic disable-next-line: undefined-global
-local jit = jit
+local finders = require 'telescope.finders'
+local make_entry = require 'telescope.make_entry'
+local pickers = require 'telescope.pickers'
+local entry_display = require 'telescope.pickers.entry_display'
+local utils = require 'telescope.utils'
+local Path = require 'plenary.path'
 
----@diagnostic disable-next-line: unused-local, unused-function
-local function logger(val)
-  print(vim.inspect(val))
-end
+local fn = vim.fn
+local api = vim.api
+local CocAction = fn.CocAction
+local CocActionAsync = fn.CocActionAsync
 
 local function is_ready(feature)
-  local is_running = vim.call('coc#client#is_running', 'coc')
-  if is_running ~= 1 then
-    return false
-  end
-  local ready = vim.call('coc#rpc#ready')
-  if ready ~= 1 then
-    return false
+  if vim.g.coc_service_initialized ~= 1 then
+    print 'Coc is not ready!'
+    return
   end
 
-  local ok = true
-  if feature then
-    ok = vim.call('CocHasProvider', feature)
-    if not ok then
-      print("Coc: server does not support " .. feature)
-    end
+  if feature and not fn.CocHasProvider(feature) then
+    print('Coc: server does not support ' .. feature)
+    return
   end
-  return ok
+
+  return true
 end
 
 local locations_to_items = function(locs)
@@ -53,8 +42,8 @@ local locations_to_items = function(locs)
     vim.fn.bufload(bufnr)
     local filename = vim.uri_to_fname(l.uri)
     local row = l.range.start.line
-    local line = (vim.api.nvim_buf_get_lines(bufnr, row, row + 1, false) or {""})[1]
-    items[#items+1] = {
+    local line = (api.nvim_buf_get_lines(bufnr, row, row + 1, false) or { '' })[1]
+    items[#items + 1] = {
       filename = filename,
       lnum = row + 1,
       col = l.range.start.character + 1,
@@ -70,7 +59,7 @@ local mru = function(opts)
     return
   end
 
-  local home = vim.call('coc#util#get_data_home')
+  local home = vim.call 'coc#util#get_data_home'
   local data = Path:new(home .. Path.path.sep .. 'mru'):read()
   if not data or #data == 0 then
     return
@@ -83,7 +72,7 @@ local mru = function(opts)
     local lowerPrefix = val:sub(1, #cwd):gsub(Path.path.sep, ''):lower()
     local lowerCWD = cwd:gsub(Path.path.sep, ''):lower()
     if lowerCWD == lowerPrefix and p:exists() and p:is_file() then
-      results[#results+1] = val:sub(#cwd+1)
+      results[#results + 1] = val:sub(#cwd + 1)
     end
   end
   pickers.new(opts, {
@@ -99,27 +88,32 @@ local mru = function(opts)
           ordinal = line,
           display = line,
         }
-      end
+      end,
     },
   }):find()
 end
 
 local links = function(opts)
-  if not is_ready('documentLink') then
+  if not is_ready 'documentLink' then
     return
   end
 
-  local res = vim.call('CocAction', 'links')
-  if type(res) ~= 'table' or vim.tbl_isempty(res) then
+  local res = CocAction 'links'
+  if type(res) ~= 'table' then
+    return
+  end
+
+  if vim.tbl_isempty(res) then
+    print 'No links available'
     return
   end
 
   local results = {}
   for _, l in ipairs(res) do
-    results[#results+1] = {
+    results[#results + 1] = {
       lnum = l.range.start.line + 1,
       col = l.range.start.character,
-      text = l.target
+      text = l.target,
     }
   end
 
@@ -134,7 +128,7 @@ local links = function(opts)
       actions.select_default:replace(function()
         actions.close(prompt_bufnr)
         local text = action_state.get_selected_entry().value.text
-        if text:find('https?://') then
+        if text:find 'https?://' then
           local opener = (jit.os == 'OSX' and 'open') or 'xdg-open'
           os.execute(opener .. ' ' .. text)
         end
@@ -146,12 +140,17 @@ local links = function(opts)
 end
 
 local handle_code_actions = function(opts, mode)
-  if not is_ready('codeAction') then
+  if not is_ready 'codeAction' then
     return
   end
 
-  local results = vim.call('CocAction', 'codeActions', mode)
-  if type(results) ~= 'table' or vim.tbl_isempty(results) then
+  local results = CocAction('codeActions', mode)
+  if type(results) ~= 'table' then
+    return
+  end
+
+  if vim.tbl_isempty(results) then
+    print 'No available code actions'
     return
   end
 
@@ -169,15 +168,15 @@ local handle_code_actions = function(opts, mode)
           valid = line ~= nil,
           value = line,
           ordinal = line.idx .. line.title,
-          display = line.idx .. ': ' .. line.title
+          display = line.idx .. ': ' .. line.title,
         }
-      end
+      end,
     },
     attach_mappings = function(prompt_bufnr)
       actions.select_default:replace(function()
         actions.close(prompt_bufnr)
         local selection = action_state.get_selected_entry()
-        vim.call('CocAction', 'doCodeAction', selection.value)
+        CocAction('doCodeAction', selection.value)
       end)
 
       return true
@@ -204,12 +203,14 @@ local function list_or_jump(opts)
     return
   end
 
-  local defs = vim.call('CocAction', opts.coc_action)
-  if type(defs) ~= 'table' or vim.tbl_isempty(defs) then
+  local defs = CocAction(opts.coc_action)
+  if type(defs) ~= 'table' then
     return
   end
 
-  if #defs == 1 then
+  if vim.tbl_isempty(defs) then
+    print(('No %s found'):format(opts.coc_action))
+  elseif #defs == 1 then
     vim.lsp.util.jump_to_location(defs[1])
   else
     local results = locations_to_items(defs)
@@ -257,11 +258,11 @@ local type_definitions = function(opts)
 end
 
 local references = function(opts)
-  if not is_ready('reference') then
+  if not is_ready 'reference' then
     return
   end
 
-  local refs = vim.call('CocAction', 'references')
+  local refs = CocAction 'references'
   if type(refs) ~= 'table' or vim.tbl_isempty(refs) then
     return
   end
@@ -272,37 +273,37 @@ local references = function(opts)
   end
 
   local make_display = function(entry)
-    local line_info = { table.concat({ entry.lnum, entry.col }, ":"), "TelescopeResultsLineNr" }
+    local line_info = { table.concat({ entry.lnum, entry.col }, ':'), 'TelescopeResultsLineNr' }
     local filename = utils.transform_path(opts, entry.filename)
 
-    local displayer = entry_display.create({
-      separator = "▏",
+    local displayer = entry_display.create {
+      separator = '▏',
       items = {
         { width = 6 },
         { width = 40 },
         { remaining = true },
       },
-    })
+    }
 
-    return displayer({
+    return displayer {
       line_info,
       filename,
-      entry.text:gsub(".* | ", ""),
-    })
+      entry.text:gsub('.* | ', ''),
+    }
   end
 
   pickers.new(opts, {
     prompt_title = 'Coc References',
     previewer = conf.qflist_previewer(opts),
     sorter = conf.generic_sorter(opts),
-    finder    = finders.new_table {
+    finder = finders.new_table {
       results = results,
       entry_maker = function(entry)
         return {
           valid = true,
 
           value = entry,
-          ordinal = (not opts.ignore_filename and entry.filename or "") .. " " .. entry.text,
+          ordinal = (not opts.ignore_filename and entry.filename or '') .. ' ' .. entry.text,
           display = make_display,
 
           filename = entry.filename,
@@ -325,7 +326,7 @@ local locations = function(opts)
     prompt_title = 'Coc Locations',
     previewer = conf.qflist_previewer(opts),
     sorter = conf.generic_sorter(opts),
-    finder    = finders.new_table {
+    finder = finders.new_table {
       results = results,
       entry_maker = opts.entry_maker or make_entry.gen_from_quickfix(opts),
     },
@@ -333,20 +334,20 @@ local locations = function(opts)
 end
 
 local document_symbols = function(opts)
-  if not is_ready('documentSymbol') then
+  if not is_ready 'documentSymbol' then
     return
   end
 
-  local current_buf = vim.api.nvim_get_current_buf()
-  local symbols = vim.call('CocAction', 'documentSymbols', current_buf)
+  local current_buf = api.nvim_get_current_buf()
+  local symbols = CocAction('documentSymbols', current_buf)
   if type(symbols) ~= 'table' or vim.tbl_isempty(symbols) then
     return
   end
 
   local results = {}
   for _, s in ipairs(symbols) do
-    results[#results+1] = {
-      filename = vim.api.nvim_buf_get_name(current_buf),
+    results[#results + 1] = {
+      filename = api.nvim_buf_get_name(current_buf),
       lnum = s.lnum,
       col = s.col,
       kind = s.kind,
@@ -358,28 +359,28 @@ local document_symbols = function(opts)
   pickers.new(opts, {
     prompt_title = 'Coc Document Symbols',
     previewer = conf.qflist_previewer(opts),
-    finder    = finders.new_table {
+    finder = finders.new_table {
       results = results,
-      entry_maker = opts.entry_maker or make_entry.gen_from_lsp_symbols(opts)
+      entry_maker = opts.entry_maker or make_entry.gen_from_lsp_symbols(opts),
     },
-    sorter = conf.prefilter_sorter{
-      tag = "symbol_type",
-      sorter = conf.generic_sorter(opts)
-    }
+    sorter = conf.prefilter_sorter {
+      tag = 'symbol_type',
+      sorter = conf.generic_sorter(opts),
+    },
   }):find()
 end
 
 local function get_workspace_symbols_requester()
   return function(prompt)
     local results = {}
-    local symbols = vim.call('CocAction', 'getWorkspaceSymbols', prompt)
+    local symbols = CocAction('getWorkspaceSymbols', prompt)
     if type(symbols) ~= 'table' or vim.tbl_isempty(symbols) then
       return results
     end
     for _, s in ipairs(symbols) do
       local filename = vim.uri_to_fname(s.location.uri)
       local kind = vim.lsp.protocol.SymbolKind[s.kind] or 'Unknown'
-      results[#results+1] = {
+      results[#results + 1] = {
         filename = filename,
         lnum = s.location.range.start.line + 1,
         col = s.location.range.start.character + 1,
@@ -394,12 +395,12 @@ end
 local workspace_symbols = function(opts)
   pickers.new(opts, {
     prompt_title = 'Coc Workspace Symbols',
-    finder    = finders.new_dynamic {
+    finder = finders.new_dynamic {
       entry_maker = opts.entry_maker or make_entry.gen_from_lsp_symbols(opts),
       fn = get_workspace_symbols_requester(),
     },
     previewer = conf.qflist_previewer(opts),
-    sorter = conf.generic_sorter()
+    sorter = conf.generic_sorter(),
   }):find()
 end
 
@@ -408,7 +409,7 @@ local diagnostics = function(opts)
     return
   end
 
-  local diagnostics = vim.call('CocAction', 'diagnosticList')
+  local diagnostics = CocAction 'diagnosticList'
   if type(diagnostics) ~= 'table' or vim.tbl_isempty(diagnostics) then
     return
   end
@@ -416,47 +417,47 @@ local diagnostics = function(opts)
   opts = opts or {}
   local results = {}
   local buf_names = {}
-  local current_buf = vim.api.nvim_get_current_buf()
-  local current_filename = vim.api.nvim_buf_get_name(current_buf)
+  local current_buf = api.nvim_get_current_buf()
+  local current_filename = api.nvim_buf_get_name(current_buf)
   if opts.get_all then
-    local bufs = vim.api.nvim_list_bufs()
-    for _,bn in ipairs(bufs) do
-      buf_names[vim.api.nvim_buf_get_name(bn)] = bn
+    local bufs = api.nvim_list_bufs()
+    for _, bn in ipairs(bufs) do
+      buf_names[api.nvim_buf_get_name(bn)] = bn
     end
   end
   for _, d in ipairs(diagnostics) do
     if opts.get_all or (d.file == current_filename) then
-      results[#results+1] = {
+      results[#results + 1] = {
         bufnr = buf_names[d.file] or current_buf,
         filename = d.file,
         lnum = d.lnum,
         col = d.col,
         start = d.location.range.start,
         finish = d.location.range['end'],
-        text = vim.trim(d.message:gsub("[\n]", "")),
+        text = vim.trim(d.message:gsub('[\n]', '')),
         type = d.severity,
       }
     end
   end
 
-  opts.path_display = utils.get_default(opts.path_display, "hidden")
+  opts.path_display = utils.get_default(opts.path_display, 'hidden')
   pickers.new(opts, {
     prompt_title = 'Coc Diagnostics',
     previewer = conf.qflist_previewer(opts),
     finder = finders.new_table {
       results = results,
-      entry_maker = opts.entry_maker or make_entry.gen_from_lsp_diagnostics(opts)
+      entry_maker = opts.entry_maker or make_entry.gen_from_lsp_diagnostics(opts),
     },
-    sorter = conf.prefilter_sorter{
-      tag = "type",
-      sorter = conf.generic_sorter(opts)
-    }
+    sorter = conf.prefilter_sorter {
+      tag = 'type',
+      sorter = conf.generic_sorter(opts),
+    },
   }):find()
 end
 
 local workspace_diagnostics = function(opts)
   opts = utils.get_default(opts, {})
-  opts.path_display = utils.get_default(opts.path_display, "shorten")
+  opts.path_display = utils.get_default(opts.path_display, 'shorten')
   opts.prompt_title = 'Coc Workspace Diagnostics'
   opts.get_all = true
   diagnostics(opts)
@@ -467,9 +468,9 @@ local commands = function(opts)
     return
   end
 
-  local cmds = vim.call("CocAction", "commands")
-  if type(cmds) ~= "table" or vim.tbl_isempty(cmds) then
-    print("No commands available")
+  local cmds = CocAction 'commands'
+  if type(cmds) ~= 'table' or vim.tbl_isempty(cmds) then
+    print 'No commands available'
     return
   end
 
@@ -483,14 +484,14 @@ local commands = function(opts)
   local make_display = function(entry)
     return displayer {
       { entry.value, 'TelescopeResultsFunction' },
-      entry.description
+      entry.description,
     }
   end
 
   pickers.new(opts, {
-    prompt_title = "Coc Commands",
+    prompt_title = 'Coc Commands',
     sorter = conf.generic_sorter(opts),
-    finder = finders.new_table({
+    finder = finders.new_table {
       results = cmds,
       entry_maker = function(line)
         return {
@@ -501,19 +502,19 @@ local commands = function(opts)
           description = line.title or '',
         }
       end,
-    }),
+    },
     attach_mappings = function(prompt_bufnr)
       actions.select_default:replace(function()
         local selection = action_state.get_selected_entry()
         actions.close(prompt_bufnr)
-        vim.call("CocActionAsync", "runCommand", selection.value)
+        CocActionAsync('runCommand', selection.value)
       end)
       return true
     end,
   }):find()
 end
 
-return require('telescope').register_extension{
+return require('telescope').register_extension {
   exports = {
     mru = mru,
     links = links,
